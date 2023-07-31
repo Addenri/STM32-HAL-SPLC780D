5e7eed48a5fe384ed7a47598d840b6f2a2337770
Andrii <andrey.eriomenko@gmail.com>
2023-07-31 23:33:39 +0300

    Initial commit

---
+++ Core/Inc/splc780d.h
@@ -0,0 +1,79 @@
+/*
+ * splc780d.h
+ *
+ *  Created on: Jul 21, 2023
+ *      Author: Andrii
+ */
+
+#ifndef INC_SPLC780D_H_
+#define INC_SPLC780D_H_
+
+#include "main.h"                   //main header must include HAL driver for your MCU
+#include "stdint.h"
+
+/*
+ * Display control pins
+*/
+#define LCD_RS				        (0x01 << 0)	//Register Select: 1 = Data (R/W), 0 = Instruction (W)
+#define LCD_RW				        (0x01 << 1)	//Register R/W Select: 1 = Read, 0 = Write
+#define LCD_E				        (0x01 << 2) //Start signal for R/W
+#define LCD_BACKLIGHT		        (0x01 << 3)
+
+#define LCD_RW_READ			        (0x01 << 1)
+#define LCD_RW_WRITE		        (0x00 << 1)
+
+#define LCD_RS_DATA			        (0x01)
+#define LCD_RS_INSTRUCTION	        (0x00)
+
+/*
+ * Control instructions
+ */
+
+/* Clear Display*/
+#define LCD_CLEAR			        (0x01)  //Write space code to all DDRAM address
+
+/* Return Home */
+#define LCD_RETURN_HOME		        (0x02)  //Set DDRAM address to 0 without DDRAM wipe
+
+/* Entry Mode Set */
+#define LCD_MOVE_CURSOR_RIGHT       (0x06)  //Move cursor right, increase DDRAM address by 1
+#define LCD_MOVE_CURSOR_LEFT        (0x04)  //Move cursor left, decrease DDRAM address by 1
+#define LCD_MOVE_DISPLAY_LEFT       (0x07)
+#define LCD_MOVE_DISPLAY_RIGHT      (0x05)
+
+/* Display ON/OFF */
+#define LCD_ON_DISPLAY              (0x0C)
+#define LCD_ON_CURSOR               (0x0A)
+#define LCD_ON_CURSOR_BLINK         (0x09)  //If Cursor Blink is "ON", no need to set Cursor "ON" bit high
+
+/* Cursor or Display Shift */
+#define LCD_SHIFT_CURSOR_LEFT       (0x10)
+#define LCD_SHIFT_CURSOR_RIGHT      (0x14)
+#define LCD_SHIFT_DISPLAY_LEFT      (0X18)
+#define LCD_SHIFT_DISPLAY_RIGHT     (0x1C)
+
+/*  Function Set */
+#define LCD_FUNC_8-BIT              (0x30)  //8-bit interface mode
+#define LCD_FUNC_2-LINE             (0x28)
+#define LCD_FUNC_SYM-5x11           (0x24)
+
+#define LCD_TO_LINE_1               (0x0C)
+#define LCD_TO_LINE_2               (0x0C)
+#define LCD_TO_LINE_3               (0x92)
+#define LCD_TO_LINE_4               (0xD4)
+
+extern I2C_HandleTypeDef *hi2c;
+
+typedef struct
+{
+	I2C_HandleTypeDef* hi2c;
+	uint8_t address;
+} LCD_HandleTypeDef;
+
+void LCD_Write(LCD_HandleTypeDef *hlcd, uint8_t Data, uint8_t Pins);
+void LCD_Read(LCD_HandleTypeDef *hlcd);
+void LCD_Init(LCD_HandleTypeDef *hlcd, I2C_HandleTypeDef *hi2c, uint8_t i2c_address);
+void LCD_Print(LCD_HandleTypeDef *hlcd, char *Text);
+void LCD_CreateSymbol(LCD_HandleTypeDef *hlcd, uint8_t Cell, uint8_t *SymbolMap);
+
+#endif /* INC_SPLC780D_H_ */
---
+++ Core/Src/splc780d.c
@@ -0,0 +1,57 @@
+/*
+ * splc780d.c
+ *
+ *  Created on: Jul 21, 2023
+ *      Author: Andrii
+ */
+
+
+#include "splc780d.h"
+
+void LCD_Write(LCD_HandleTypeDef *hlcd, uint8_t Data, uint8_t Pins)
+{
+  uint8_t LCD_Command[4] = {0, 0, 0, 0};
+  uint8_t High = Data & 0xF0;
+  uint8_t Low = (Data << 4) & 0xF0;
+
+  LCD_Command[0] = High | Pins | LCD_BACKLIGHT | LCD_E;
+  LCD_Command[1] = High | Pins | LCD_BACKLIGHT;
+  LCD_Command[2] = Low | Pins | LCD_BACKLIGHT | LCD_E;
+  LCD_Command[3] = Low | Pins | LCD_BACKLIGHT;
+
+  HAL_I2C_Master_Transmit(hlcd->hi2c, hlcd->address, (uint8_t*)&LCD_Command, sizeof(LCD_Command), HAL_MAX_DELAY);
+  HAL_Delay(5);
+}
+
+void LCD_Init(LCD_HandleTypeDef *hlcd, I2C_HandleTypeDef *hi2c, uint8_t i2c_address)
+{
+  hlcd->hi2c = hi2c;
+  hlcd->address = (i2c_address << 1);   //STM32 HAL use 1-bit shifted address
+  HAL_Delay(40);
+  if(HAL_I2C_IsDeviceReady(hlcd->hi2c, hlcd->address, 1, HAL_MAX_DELAY) != HAL_OK)
+	Error_Handler();
+  LCD_Write(&hlcd, LCD_CLEAR, LCD_RS_INSTRUCTION);
+}
+
+void LCD_Print(LCD_HandleTypeDef *hlcd, char *Text)
+{
+  while(*Text)
+  {
+    if(*Text == '\n')
+
+      //LCD_Write(hlcd, 0xC0, LCD_RS_INSTRUCTION);
+    else
+      LCD_Write(hlcd, (uint8_t)(*Text), LCD_RS);
+    Text++;
+  }
+}
+
+void LCD_CreateSymbol(LCD_HandleTypeDef *hlcd, uint8_t Cell, uint8_t *SymbolMap)
+{
+  Cell &= 0x7;
+  LCD_Write(hlcd, (Cell << 3), LCD_RS_INSTRUCTION);
+  for(int i = 0; i < 8; i++)
+  {
+    LCD_Write(hlcd, SymbolMap[i], LCD_RS_DATA);
+  }
+}
